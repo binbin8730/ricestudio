7327cb53c6dfbb4cdcf1e6e79d96036915dbc1e1
Wen bin <905613455@qq.com>
2018-08-12 15:41:06 +0000

    User can create topic

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -37,7 +37,12 @@
                             <button type="button" class="btn btn-cta btn-cta-primary" data-toggle="modal" data-target="#signup-modal"><a href="{{ route('register') }}">注册</a></button>
                         </li>
                     @else
-                        <li class="nav-item dropdown">
+                        <li class="nav-item nav-create " >
+                            <a href="{{ route('topics.create') }}">
+                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                            </a>
+                        </li>
+                        <li class="nav-item dropdown last">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
                                <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
