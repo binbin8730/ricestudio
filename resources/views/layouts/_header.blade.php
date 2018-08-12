706d875442fb0d5be95dc4afc8cc9971424dcb16
Wen bin <905613455@qq.com>
2018-08-12 14:49:42 +0000

    Category topic list

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -16,16 +16,16 @@
             </div><!--//navbar-header-->
             <div class="navbar-collapse collapse" id="navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <li class="active nav-item"><a href="{{ url('/') }}">首页</a></li>
+                    <li class="nav-item {{ active_class(if_route('root')) }}"><a href="{{ url('/') }}">首页</a></li>
                     <li class="nav-item"><a href="tour.html">浏览</a></li>
                     <li class="nav-item"><a href="pricing.html">价格</a></li>
-                    <li class="nav-item dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false" href="blog.html">博客 <i class="fa fa-angle-down"></i></a>
+                    <li class="nav-item  {{ active_class(if_route('categories.show')) }}  dropdown ">
+                        <a class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false" href="{{ route('topics.index') }}">话题 <i class="fa fa-angle-down"></i></a>
                         <ul class="dropdown-menu">
-                            <li><a href="blog.html">Blog Home</a></li>
-                            <li><a href="blog-single.html">Single Blog Post</a></li>
-                            <li><a href="404.html">404 Page</a></li>
-                            <li><a href="email-templates/tempo-email-color-1.html">Email Template (10 Colour Options)</a></li>
+                            <li><a href="{{ route('categories.show', 1) }}">分享</a></li>
+                            <li><a href="{{ route('categories.show', 2) }}">教程</a></li>
+                            <li><a href="{{ route('categories.show', 3) }}">问答</a></li>
+                            <li><a href="{{ route('categories.show', 4) }}">公告</a></li>
                         </ul>
                     </li><!--//dropdown-->
                     <li class="nav-item"><a href="contact.html">联系我们</a></li>
