3e050bb6a14998f8e843246f2901f831152bb86e
Wen bin <905613455@qq.com>
2018-08-13 04:09:29 +0000

    Topic detaile page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>@yield('title', 'Rice Studio') - 水稻工作室</title>
-
+        <meta name="description" content="@yield('description', 'Rice Studio 水稻工作室')" />
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         <link href="{{ asset('plugins/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
